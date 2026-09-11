68e34359c621715610654cd26f4fdf60c48497e9
dingyi <dingyi222666@foxmail.com>
2026-09-11 17:56:06 +0800

    chore: register v0.1.18 in the backfill release table

--- scripts/backfill-releases.ts
+++ scripts/backfill-releases.ts
@@ -40,6 +40,7 @@ const VERSIONS: ReadonlyArray<readonly [version: string, commit: string]> = [
   ['0.1.15', '0e15149'],
   ['0.1.16', 'd272a873f'],
   ['0.1.17', 'f94d45d76'],
+  ['0.1.18', '606d6187d'],
 ]
 
 /** GitHub logins by git author email. */
